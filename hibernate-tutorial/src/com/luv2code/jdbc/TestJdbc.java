4a797cd27af18f52ada52f2f2b5f7a498e04079c
Dang Nguyen <nguyenmanhdang90@gmail.com>
2019-05-09 22:38:27 +0700

    hibernate tutorial

---
+++ hibernate-tutorial/src/com/luv2code/jdbc/TestJdbc.java
@@ -0,0 +1,22 @@
+package com.luv2code.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+	public static void main(String[] args) {
+		String jdbcUrl = "jdbc:mysql://localhost:3306/hb_student_tracker?useSSL=false&serverTimezone=UTC";
+		String user = "hbstudent";
+		String password = "hbstudent";
+
+		try {
+			Connection myConn = DriverManager.getConnection(jdbcUrl, user, password);
+			System.out.println("Connection successful");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
